17d20e2890946f173335801cb5e1a64ffa23064a
Isadora <isadora-gf@hotmail.com>
2023-05-23 09:48:34 +0200

    adicionando formulario

--- base/views.py
+++ base/views.py
@@ -3,3 +3,12 @@ from django.http import HttpResponse
 # Create your views here.
 def inicio(request):
     return render(request, 'inicio.html')
+
+def contato(request):
+    contexto = {
+        'telefone': '999999',
+        'responsavel': 'Maria'
+    }
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'contato.html', contexto)
--- ultima/urls.py
+++ ultima/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from base.views import inicio 
+from base.views import inicio, contato
 
 urlpatterns = [
+    path('contato/', contato),
     path('', inicio),
     path('admin/', admin.site.urls),
 ]
