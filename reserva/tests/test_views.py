51d4d0dc6387135b7cf55d5673f4f9309b75693b
Isadora <isadora-gf@hotmail.com>
2023-07-18 12:34:17 +0200

    tests

--- reserva/tests/test_views.py
+++
@@ -1,6 +0,0 @@
-from pytest_django.asserts import assertTemplateUsed
-
-def test_reserva_view_deve_retornar_template(client):
-    response = client.get('/reserva/criar/')
-    assert response.status_code == 200
-    assertTemplateUsed(response, 'criar_reserva.html')
